ca3de3625cb428ca2afbedea3236237f874e9fa9
Tapan Ghosh <tapang786@gmail.com>
2026-02-22 22:44:36 +0530

    update

--- android/app/src/main/kotlin/com/invisofts/mehndi_designs/MainActivity.kt
+++ android/app/src/main/kotlin/com/invisofts/mehndi_designs/MainActivity.kt
@@ -5,9 +5,4 @@ import androidx.activity.enableEdgeToEdge
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Enable edge-to-edge display for Android 15+
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-    }
 }
